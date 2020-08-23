17f5ee9f95c9a8f6ed27b39c51cedd5c01fe9c6b
Dane David <dane@surveysparrow.com>
2020-08-24 02:40:54 +0530

    Updates

---
+++ precache-manifest.1a5733766cca0a54eedbdb7890becb69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dc47748d0ec511358c1e52c6bcc0bc",
+    "url": "/react-datatable/index.html"
+  },
+  {
+    "revision": "dd766177d10c4c6877cf",
+    "url": "/react-datatable/static/css/main.ab4cd4e6.chunk.css"
+  },
+  {
+    "revision": "611f0dc80688be7e2dea",
+    "url": "/react-datatable/static/js/2.d2ff8b33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-datatable/static/js/2.d2ff8b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd766177d10c4c6877cf",
+    "url": "/react-datatable/static/js/main.8d0d5616.chunk.js"
+  },
+  {
+    "revision": "b23c09466dedd78dd3cc",
+    "url": "/react-datatable/static/js/runtime-main.bf82f366.js"
+  }
+]);
